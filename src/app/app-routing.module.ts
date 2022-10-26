eb4a0ffef4120078f1ee734c832abbe69a9fb08a
Devin-Jackson314 <you@example.com>
2022-10-26 15:32:22 -0400

    setting up routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InvoicesComponent } from './components/invoices/invoices.component';
+import { ViewInvoiceComponent } from './components/view-invoice/view-invoice.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   { path: 'invoices', component: InvoicesComponent },
+  { path: 'view', component: ViewInvoiceComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
