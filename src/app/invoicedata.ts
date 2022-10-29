f7a05a0d11ff166f08d091e07ac933d55684b8bb
Devin-Jackson314 <you@example.com>
2022-10-29 18:23:55 -0400

    created navbar compnent

---
+++ src/app/invoicedata.ts
@@ -0,0 +1,16 @@
+export interface Invoicedata {
+    clientAddress: {
+       city:
+    }
+    city: string;
+    country: string;
+    postCode: string;
+    street: string;
+    clientEmail: string;
+    clientName: string;
+    createdAt: string;
+    description: string;
+    id: string;
+
+
+}
