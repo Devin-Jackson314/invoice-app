0e0f52f9ffdaa14862edad603a7f2b4356051606
Devin-Jackson314 <you@example.com>
2022-11-02 11:23:10 -0400

    added new invoice to my invoice reducer

--- src/app/store/reducers/invoices.reducer.ts
+++ src/app/store/reducers/invoices.reducer.ts
@@ -1,5 +1,6 @@
 import { createReducer, on } from '@ngrx/store';
 import { loadInvoicessSuccess } from '../actions/invoices.actions';
+import { newInvoiceSuccess } from '../actions/newinvoice.actions';
 
 
 
@@ -11,6 +12,12 @@ export const initialState: object = {
 export const InvoicesReducer = createReducer(
   initialState,
 
-  on(loadInvoicessSuccess, (State, invoices) => invoices)
+  on(loadInvoicessSuccess, (State, invoices) => invoices),
+
+  on(newInvoiceSuccess, (state, invoices) => {
+        const payload = {...invoices}
+        delete payload.uuid
+        return {...state, [invoices.uuid.name]:payload}
+    })
 
 );
