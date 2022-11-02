a8f8562758014aaa20e5d9e82d91c872573c6bb3
Devin-Jackson314 <you@example.com>
2022-11-02 08:36:35 -0400

    setting up my form controls

--- src/app/components/view-invoice/view-invoice.component.ts
+++ src/app/components/view-invoice/view-invoice.component.ts
@@ -4,15 +4,39 @@ import { DataService } from 'src/app/services/data.service';
 import { InvoicesSelector } from 'src/app/store/selectors/invoices.selectors';
 import { viewInvoice } from 'src/app/store/selectors/viewinvoices.selectors';
 import { ActivatedRoute } from '@angular/router';
+import { FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-view-invoice',
   templateUrl: './view-invoice.component.html',
   styleUrls: ['./view-invoice.component.css']
 })
 export class ViewInvoiceComponent implements OnInit {
-   myInvoice$ : any;
+  myInvoice$: any;
+  
+  invoiceForm = this.formBuilder.group({
+    BillersAddress: '',
+    City: '',
+    PostCode: '',
+    Country: '',
+    ClientsName: '',
+    ClientsEmail: '',
+    ClientAddress: '',
+    ClientsCity: '',
+    ClientPostCode: '',
+    ClientsCountry: '',
+    InvoiceDate: '',
+    PaymentTerms: '',
+    ProjectDescription: '',
+    ItemName: '',
+    Qty: '',
+    Price: '',
+    Total: '',
+    ItemName2: '',
 
-  constructor(private dataservice: DataService, private store: Store, private route: ActivatedRoute) { }
+  });
+ 
+
+  constructor(private dataservice: DataService, private store: Store, private route: ActivatedRoute, private formBuilder: FormBuilder) { }
 
  
   
