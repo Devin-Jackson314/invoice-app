2554f7fbb4c15f7858ddf5352d1f96272d94cd85
Devin-Jackson314 <you@example.com>
2022-11-03 11:20:15 -0400

    created edit invoices action

---
+++ src/app/store/actions/editinvoices.actions.ts
@@ -0,0 +1,12 @@
+import { createAction, props } from "@ngrx/store";
+import { Invoices } from "src/app/invoicedata";
+
+export const invokeEdit = createAction(
+    '[Invoices] Invoke edit Invoices',
+    props<{ editInvoice: Invoices }>()
+);
+
+export const invokeEditSuccess = createAction(
+    '[Invoices] Invoke edit sucess',
+    props<{editInvoice: Invoices}>
+);
